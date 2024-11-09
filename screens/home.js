659cf271d0758030e1dc0c523b0923f95703804f
JorgeAngelesTorres <165735853+JorgeAngelesTorres@users.noreply.github.com>
2024-11-09 06:00:22 +0000

    mis cambios

--- screens/home.js
+++ screens/home.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+//import { useNavigation } from "@react-navigation/native";
 
-const home = () => {
+const home = ({ route, navigation }) => {
+  const email = route.params.email;
+  const auth = getAuth();
+  //const navigation = useNavigation();
+
+  const logoutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Signed out!");
+        navigation.navigate("login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const categoriesHandler = () => {
+    navigation.navigate("CategoriesScreen");
+  };
+
+  const Drawer = createDrawerNavigator();
+
   return (
-    <View>
-      <Text>home</Text>
+    <View style={styles.container}>
+      <Text>Welcome {email}</Text>
+
+      <Button title="Logout" onPress={logoutHandler} />
+
+      <Button title="Categories" onPress={categoriesHandler} />
+
     </View>
-  )
-}
+  );
+};
 
-export default home
+export default home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
